30b9f55af5926966c2a6db8c3d2201cee47ecb4b
guycdev <cohenguy74@gmail.com>
2024-12-06 23:28:36 -0500

    deploy config

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   envDir: "src/env/",
+  base: "Rarepath-home",
   plugins: [react()],
   resolve: {
     alias: {
