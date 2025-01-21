d6e8090a3b0a54b35fcdb7699543baaad8c65a1a
Sundareswar Pullela <110754869+sundareswarpullela@users.noreply.github.com>
2025-01-21 14:56:46 -0800

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   envDir: "src/env/",
-  base: "/<REPO>/",
+  base: "/",
   plugins: [react()],
   resolve: {
     alias: {
